5cabb8e9362dc15f5520a9b50c858d0d8e5f3e86
bridge <bertassilaura@gmail.com>
2026-05-20 17:28:47 -0300

    outra tentativa

--- v740/js/busca.js
+++ v740/js/busca.js
@@ -1,6 +1,3 @@
----
-layout: null
----
 (function() {  
   // Inicializando o lunr com os campos em que irá pesquisar.
   var idx = lunr(function () {
@@ -44,7 +41,7 @@ layout: null
 
       searchResults.innerHTML += appendString;
     } else {
-      searchResults.innerHTML += '<div align="center"><img src="{{ site.baseurl }}/img/searchNoResults.png"></div>';
+      searchResults.innerHTML += '<div align="center"><img src="/v740/img/searchNoResults.png"></div>';
     }
   }
 
